06158562c277f4544f133e0259c81be31f05b3c8
xoreo <mattnappo@gmail.com>
2020-07-01 17:26:59 -0400

    Updated tex compilation script.

--- Notes/compile_tex.sh
+++ Notes/compile_tex.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export name='notes'
-pdflatex $name.tex
-rm $name.log
-open notes.pdf
-
+mkdir -p out
+pdflatex -output-directory out/ $1
+rm out/${1%.tex}.log
+rm out/texput.log
+xdg-open out/${1%.tex}.pdf
