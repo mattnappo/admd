f08158ffba03fd7aa2a5d9933e7dfce3c5b41914
xoreo <mattnappo@gmail.com>
2020-11-16 20:10:35 -0500

    Fixed notes.

--- Notes/compile_tex.sh
+++ Notes/compile_tex.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mkdir -p out
-pdflatex -output-directory out/ $1 # && xdg-open out/${1%.tex}.pdf
+pdflatex -output-directory out/ $1 && xdg-open out/${1%.tex}.pdf
 rm out/${1%.tex}.log
